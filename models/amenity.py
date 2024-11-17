3456697ff34ef77566f698056bf515984983e894
ouidane <ouidanezakaria@gmail.com>
2024-11-17 15:02:36 +0100

    Amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""Defines Amenity Class."""
+"""Amenity Class module."""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represents an amenity.
-
-    Attributes:
-        name (str): The name of the amenity.
-    """
+    """Represents an amenity."""
 
     name = ""
