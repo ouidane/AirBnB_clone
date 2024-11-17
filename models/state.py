8cef17b104535337e04d2b43925001ae1acd4807
ouidane <ouidanezakaria@gmail.com>
2024-11-17 14:47:31 +0100

    update state class

--- models/state.py
+++ models/state.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """Defines the State Class."""
+
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represents a geographical state.
-
-    Attributes:
-        name (str): The name of the geographical state.
-    """
+    """Represents a geographical state."""
 
     name = ""
+
