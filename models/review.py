e6d4dcbc7324b91cc8c2c7f75cb94cad77844870
ouidane <ouidanezakaria@gmail.com>
2024-11-17 14:51:11 +0100

    Review class

--- models/review.py
+++ models/review.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""Defines Review Class."""
+"""Review Class module."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Represents a review in the HBnB application.
-
-    Attributes:
-        place_id (str): The ID of the place associated with the review.
-        user_id (str): The ID of the user who wrote the review.
-        text (str): The content of the review.
-    """
+    """Represents a review."""
 
     place_id = ""
     user_id = ""
