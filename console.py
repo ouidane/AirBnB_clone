a4cd1f0818c5cccac050c7d5a887d214a5b605de
ouidane <ouidanezakaria@gmail.com>
2024-11-18 00:17:11 +0100

    update console

--- console.py
+++ console.py
@@ -13,11 +13,11 @@ from models.state import State
 from models.user import User
 
 
-def str_parse(arg):
-    """Parse The input string argument into a list.
+def handle_cmd(arg):
+    """Parse a string argument and return a list of items.
 
     Args:
-        arg (str): The string to be parsed.
+        arg (str): The input string to be parsed..
     """
     curly_braces = re.search(r"\{(.*?)\}", arg)
     brackets = re.search(r"\[(.*?)\]", arg)
@@ -37,7 +37,13 @@ def str_parse(arg):
 
 
 class HBNBCommand(cmd.Cmd):
-    """Implementation of the HBnB command-line interface."""
+    """Implementation of the HBnB command-line interface.
+
+    Attributes:
+        prompt (str): The prompt displayed in the command line.
+        __classes (set): A set of known classes including "BaseModel,"
+            "Amenity," "City," "Place," "Review," "State," and "User."
+    """
 
     prompt = "(hbnb) "
     __classes = {
@@ -51,7 +57,7 @@ class HBNBCommand(cmd.Cmd):
         }
 
     def emptyline(self):
-        """Do nothings when receiving an empty line."""
+        """Do nothins when receiving an empty line."""
         pass
 
     def default(self, arg):
@@ -59,6 +65,9 @@ class HBNBCommand(cmd.Cmd):
 
         Args:
             arg (str): The unrecognized command.
+
+        Returns:
+            bool: False if the command is not recognized.
         """
         default_args = {
             "all": self.do_all,
@@ -89,8 +98,12 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, arg):
-        """Create a new instance and print its id."""
-        list_args = str_parse(arg)
+        """
+        Usage: create <class>
+
+        Create a new instance and print its `id`.
+        """
+        list_args = handle_cmd(arg)
         if len(list_args) == 0:
             print("** class name missing **")
             return False
@@ -101,8 +114,12 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
 
     def do_show(self, arg):
-        """Display the string representation of an instance of a given id."""
-        list_args = str_parse(arg)
+        """
+        Usage: show <class> <id> or <class>.show(<id>)
+
+        Display the string representation of an instance of a given id.
+        """
+        list_args = handle_cmd(arg)
         objs = storage.all()
         if len(list_args) == 0:
             print("** class name missing **")
@@ -119,8 +136,12 @@ class HBNBCommand(cmd.Cmd):
         print(objs["{}.{}".format(list_args[0], list_args[1])])
 
     def do_destroy(self, arg):
-        """Delete the instance of the given id."""
-        list_args = str_parse(arg)
+        """
+        Usage: destroy <class> <id> or <class>.distroy(<id>)
+
+        Delete the instance of the given id.
+        """
+        list_args = handle_cmd(arg)
         objs = storage.all()
         if len(list_args) == 0:
             print("** class name missing **")
@@ -138,8 +159,13 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
 
     def do_all(self, arg):
-        """Display all instances of a given class as a string."""
-        list_args = str_parse(arg)
+        """
+        Usage: all or all <class> or <class>.all()
+
+        Display string representations of all instances of a given class.
+        If no class is specified, displays all instantiated objects.
+        """
+        list_args = handle_cmd(arg)
         if len(list_args) > 0 and list_args[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
             return False
@@ -153,8 +179,15 @@ class HBNBCommand(cmd.Cmd):
             print(list_objs)
 
     def do_update(self, arg):
-        """Update the instance of a given id."""
-        list_args = str_parse(arg)
+        """
+        Usage: update <class name> <id> <attribute name> "<attribute value>" or
+        <class>.update(<id>, <attribute name>, "<attribute value>") or
+        <class>.update(<id>, <dictionary>).
+
+        Update the instance of a given id by adding or
+        updating a given attribute.
+        """
+        list_args = handle_cmd(arg)
         objs = storage.all()
         if len(list_args) == 0:
             print("** class name missing **")
@@ -198,8 +231,12 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
 
     def do_count(self, arg):
-        """Retrive the number of instances of a given class."""
-        list_args = str_parse(arg)
+        """
+        Usage: count <class> or <class>.count()
+
+        Retrive the number of instances of a given class.
+        """
+        list_args = handle_cmd(arg)
         count = 0
         for obj in storage.all().values():
             if list_args[0] == obj.__class__.__name__:
